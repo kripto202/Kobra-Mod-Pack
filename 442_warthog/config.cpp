5ea32b60c1e9b6a504c4aefc7f3287ef92a187ed
mjr121 <33104614+mjr121@users.noreply.github.com>
2020-06-13 17:43:09 -0500

    fix
    fix

--- 442_warthog/config.cpp
+++ 442_warthog/config.cpp
@@ -14,8 +14,6 @@ class cfgPatches
 			"rep_warthog_mg",
 			"rep_warthog_at",
 			"rep_warthog_tt",
-<<<<<<< Updated upstream
-=======
 			"cis_warthog_mg",
 
 			"bl_warthog_unarmed",
@@ -34,7 +32,6 @@ class cfgPatches
 			"442_deserter_warthog_mg_woodland",
 			"442_deserter_warthog_at_woodland",
 			"442_deserter_warthog_tt_woodland"
->>>>>>> Stashed changes
 		};
 		requiredaddons[] = {};
 	};
@@ -57,16 +54,11 @@ class cfgVehicles
 {
 	class OPTRE_M12_FAV;
 	class OPTRE_M12_LRV;
-<<<<<<< Updated upstream
 	class OPTRE_M12A1_LRV;
 	class OPTRE_M12G1_LRV;
 	class OPTRE_M12R_AA;
-	class OPTRE_M813_TT;
 	class OPTRE_M914_RV;
-	
-=======
 	class OPTRE_M813_TT;
-	class OPTRE_M12A1_LRV;
 	class Turrets;
 	class MainTurret;
 	class HitPoints;
@@ -75,7 +67,6 @@ class cfgVehicles
 	class LandVehicle;
 	class AnimationSources;
 //Unarmed
->>>>>>> Stashed changes
 	class 442_warthog_unarmed: OPTRE_M12_FAV
 	{
 		displayname = "[442] Warthog (Unarmed)";
@@ -101,216 +92,7 @@ class cfgVehicles
 			"OPTRE_Vehicles\Warthog\data\warthog_addons_snow_co.paa"
 		};
 	};
-<<<<<<< Updated upstream
 	class 442_warthog_mg: OPTRE_M12_LRV
-=======
-	class rep_warthog_unarmed: OPTRE_M12_FAV
-	{
-		displayname = "Warthog (Unarmed)";
-		driverAction = "Driver_Warthog";
-		scope = 2;
-		side = 1;
-		crew = "SWLB_clone_base_P2";
-		faction = "kobra";
-		editorsubcategory = "442_rep_warthog";
-		hiddenSelections[] = 
-		{
-			"camo1",
-			"camo2",
-			"clan",
-			"clan_text",
-			"insignia",
-			"attach_apc",
-			"attach_mg",
-			"attach_troop"
-		};
-		hiddenSelectionsTextures[] = 
-		{
-			"442_warthog\rep_warthog.paa",
-			"OPTRE_Vehicles\Warthog\data\warthog_addons_snow_co.paa"
-		};
-	};
-	class bl_warthog_unarmed: OPTRE_M12_FAV
-	{
-		displayname = "[bl] Warthog (Unarmed)";
-		scope = 2;
-		side = 1;
-		driverAction = "Driver_Warthog";
-		crew = "SWLA_BL_Trooper";
-		faction = "black_legion";
-		editorsubcategory = "bl_optre_vehicles";
-		hiddenSelections[] = 
-		{
-			"camo1",
-			"camo2",
-			"clan",
-			"clan_text",
-			"insignia",
-			"attach_apc",
-			"attach_mg",
-			"attach_troop"
-		};
-		hiddenSelectionsTextures[] = 
-		{
-			"442_warthog\bl_warthog.paa",
-			"OPTRE_Vehicles\Warthog\data\warthog_addons_black_co.paa"
-		};
-	};
-//MG
-	class 442_warthog_mg_Base: OPTRE_M12_LRV
-	{
-		scope = 0;
-		displayName = "M12 LRV Warthog (MG)";
-		author = "mjr121";
-		faction = "kobra";
-		model = "OPTRE_Vehicles\Warthog\Warthog_MG.p3d";
-		transportSoldier = 0;
-		hiddenSelections[] = {"Camo1","Camo2","clan","clan_text","insignia","_Ammobox","_Axe","_Cans","_FireExtinguisher","_Shovel"};
-		hiddenSelectionsTextures[] = {"OPTRE_Vehicles\Warthog\data\warthog_co.paa","OPTRE_Vehicles\Warthog\data\warthog_addons_co.paa"};
-		driverAction = "Driver_Warthog";
-		class RenderTargets
-		{
-			class FrontView
-			{
-				renderTarget = "rendertarget0";
-				class CameraView1
-				{
-					pointPosition = "PIP0_pos";
-					pointDirection = "PIP0_dir";
-					renderQuality = 2;
-					renderVisionMode = 0;
-					fov = 0.7;
-				};
-			};
-			class RearView
-			{
-				renderTarget = "rendertarget1";
-				class CameraView1
-				{
-					pointPosition = "PIP1_pos";
-					pointDirection = "PIP1_dir";
-					renderQuality = 2;
-					renderVisionMode = 0;
-					fov = 0.7;
-				};
-			};
-			class leftMirror
-			{
-				renderTarget = "rendertarget2";
-				class CameraView1
-				{
-					pointPosition = "PIP2_pos";
-					pointDirection = "PIP2_dir";
-					renderQuality = 2;
-					renderVisionMode = 0;
-					fov = 0.7;
-				};
-			};
-			class rightMirror
-			{
-				renderTarget = "rendertarget3";
-				class CameraView1
-				{
-					pointPosition = "PIP3_pos";
-					pointDirection = "PIP3_dir";
-					renderQuality = 2;
-					renderVisionMode = 0;
-					fov = 0.7;
-				};
-			};
-			class GunScreen
-			{
-				renderTarget = "rendertarget4";
-				class CameraView1
-				{
-					pointPosition = "PIP4_pos";
-					pointDirection = "PIP4_dir";
-					renderQuality = 2;
-					renderVisionMode = 0;
-					fov = 0.5;
-				};
-			};
-		};
-		class Turrets: Turrets
-		{
-			class CargoGunner_1: CargoGunner_1{};
-			class MainTurret: MainTurret
-			{
-				body = "mainturret";
-				gun = "maingun";
-				animationsourcebody = "mainturret";
-				animationSourceGun = "maingun";
-				gunAxis = "Osa Hlavne";
-				gunBeg = "Usti hlavne";
-				gunEnd = "konec hlavne";
-				LODTurnedOut = 1100;
-				LODTurnedIn = 1100;
-				LODOpticsIn = 1100;
-				LODOpticsOut = 1100;
-				minElev = -15;
-				maxElev = 30;
-				minTurn = -360;
-				maxTurn = 360;
-				initTurn = 0;
-				turretAxis = "OsaVeze";
-				maxHorizontalRotSpeed = 1.75;
-				maxVerticalRotSpeed = 1.5;
-				gunnerAction = "Gunner_MG_Warthog";
-				gunnerInAction = "Gunner_MG_Warthog";
-				gunnerGetInAction = "GetInMRAP_01";
-				gunnerGetOutAction = "GetOutMRAP_01";
-				gunnerName = "Gunner";
-				hideWeaponsGunner = 0;
-				soundServo[] = {"",0.01,1};
-				stabilizedInAxes = 3;
-				outGunnerMayFire = 1;
-				inGunnerMayFire = 1;
-				commanding = 1;
-				primaryGunner = 1;
-				turretInfoType = "RscWeaponZeroing";
-				memoryPointsGetInGunner = "Pos Gunner";
-				memoryPointsGetInGunnerDir = "Pos Gunner dir";
-				gunnerLeftHandAnimName = "OtocHlaven";
-				gunnerRightHandAnimName = "OtocHlaven";
-				memoryPointGun = "machinegun";
-				weapons[] = {"rep_Mounted_repeater"};
-				magazines[] = {"rep_1000Rnd_Mounted_repeater_cells","rep_1000Rnd_Mounted_repeater_cells","rep_1000Rnd_Mounted_repeater_cells"};
-				memoryPointGunnerOptics = "gunnerview";
-				memoryPointGunnerOutOptics = "gunnerview";
-				gunnerOpticsShowCursor = 1;
-				castGunnerShadow = 1;
-				startEngine = 0;
-				enableManualFire = 0;
-				gunnerOpticsModel = "a3\weapons_f\Reticle\optics_empty";
-				gunnerOutOpticsModel = "a3\weapons_f\Reticle\optics_empty";
-				gunnerForceOptics = 1;
-				hasGunner = 1;
-				class ViewGunner: ViewOptics
-				{
-					initAngleX = -15;
-					minAngleX = -45;
-					maxAngleX = 45;
-					initFov = 0.9;
-					minFov = 0.42;
-					maxFov = 0.9;
-					visionMode[] = {};
-				};
-			};
-		};
-		class AnimationSources: AnimationSources
-		{
-			class Gatling
-			{
-				source = "revolving";
-				weapon = "rep_Mounted_repeater";
-			};
-		};
-		supplyRadius = 5;
-		transportMaxMagazines = 120;
-		maximumLoad = 400;
-	};
-	class 442_warthog_mg: 442_warthog_mg_Base
->>>>>>> Stashed changes
 	{
 		displayname = "[442] Warthog (MG)";
 		scope = 2;
@@ -340,8 +122,6 @@ class cfgVehicles
 			"OPTRE_Vehicles\Warthog\data\warthog_addons_snow_co.paa"
 		};
 	};
-<<<<<<< Updated upstream
-=======
 	class bl_warthog_mg: 442_warthog_mg
 	{
 		displayname = "[BL] Warthog (MG)";
@@ -359,7 +139,6 @@ class cfgVehicles
 		};
 	};
 //TT
->>>>>>> Stashed changes
 	class 442_warthog_tt: OPTRE_M813_TT
 	{
 		displayname = "[442] Warthog (Transport)";
@@ -401,28 +180,7 @@ class cfgVehicles
 			"OPTRE_Vehicles\Warthog\data\warthog_addons_snow_co.paa"
 		};
 	};
-<<<<<<< Updated upstream
 	class rep_warthog_mg: OPTRE_M12_LRV
-=======
-	class bl_warthog_tt: OPTRE_M813_TT
-	{
-		displayname = "[BL] Warthog (Transport)";
-		scope = 2;
-		side = 1;
-		driverAction = "Driver_Warthog";
-		crew = "SWLA_BL_Trooper";
-		faction = "black_legion";
-		editorsubcategory = "bl_optre_vehicles";
-		hiddenSelections[] = {"camo1","camo2","clan","clan_text","insignia","attach_apc","attach_mg","attach_antenna"};
-		hiddenSelectionsTextures[] = 
-		{
-			"442_warthog\bl_warthog.paa",
-			"OPTRE_Vehicles\Warthog\data\warthog_addons_black_co.paa"
-		};
-	};
-//AT
-	class 442_warthog_at: OPTRE_M12A1_LRV
->>>>>>> Stashed changes
 	{
 		displayname = "Warthog (MG)";
 		scope = 2;
@@ -452,28 +210,7 @@ class cfgVehicles
 			"OPTRE_Vehicles\Warthog\data\warthog_addons_snow_co.paa"
 		};
 	};
-<<<<<<< Updated upstream
 	class rep_warthog_tt: OPTRE_M813_TT
-=======
-	class bl_warthog_at: OPTRE_M12A1_LRV
-	{
-		displayname = "[BL] Warthog (Rocket)";
-		scope = 2;
-		side = 1;
-		driverAction = "Driver_Warthog";
-		crew = "SWLA_BL_Trooper";
-		faction = "black_legion";
-		editorsubcategory = "bl_optre_vehicles";
-		hiddenSelections[] = {"camo1","camo2","clan","clan_text","insignia"};
-		hiddenSelectionsTextures[] = 
-		{
-			"442_warthog\bl_warthog.paa",
-			"OPTRE_Vehicles\Warthog\data\warthog_addons_black_co.paa"
-		};
-	};
-//CIS hogs
-	class cis_warthog_mg : 442_warthog_mg
->>>>>>> Stashed changes
 	{
 		displayname = "Warthog (Transport)";
 		scope = 2;
