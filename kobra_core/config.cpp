a4caf8b745be1eb7bd219bc92cd7a6c2b1ed6e05
kripto202 <kripto202@yahoo.com>
2020-08-12 19:19:38 -0400

    fixing the icons to show up for magazines

--- kobra_core/config.cpp
+++ kobra_core/config.cpp
@@ -1794,3 +1794,18 @@ class cfgmagazines
 	#include "442_weapons\magazine\shotgun_magazine.hpp"
 	#include "442_weapons\magazine\rocket_magazine.hpp"
 };
+
+class CfgVehicleIcons
+{
+	442_iconMan="\A3\ui_f\data\map\vehicleicons\iconMan_ca.paa";
+    442_iconManMedic="kobra_core\442_icons\data\icon_medic.paa";
+    442_iconManEngineer="kobra_core\442_icons\data\icon_engineer.paa";
+    442_iconManExplosive="kobra_core\442_icons\data\icon_engineer.paa";
+    442_iconManRecon="kobra_core\442_icons\data\icon_marksman.paa";
+	442_iconManAT="kobra_core\442_icons\data\icon_at.paa";
+    442_iconManLeader="kobra_core\442_icons\data\icon_leader.paa";
+    442_iconManMG="kobra_core\442_icons\data\icon_mg.paa";
+    442_iconManOfficer="kobra_core\442_icons\data\icon_leader.paa";
+	pictureHeal="\A3\ui_f\data\map\vehicleicons\pictureHeal_ca.paa";
+	pictureRepair="\A3\ui_f\data\map\vehicleicons\pictureRepair_ca.paa";
+};
