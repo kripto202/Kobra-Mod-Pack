c106b3c956a8944ae503887f5bbdb959ac197e27
kripto202 <kripto202@yahoo.com>
2020-08-28 02:48:19 -0400

    add 44th laat design

--- kobra_core/442_a_vehicle/laat_designs.hpp
+++ kobra_core/442_a_vehicle/laat_designs.hpp
@@ -6,23 +6,40 @@ class TextureSources
                 author = "kripto202";
                 textures[] =
                 {
-                    "442_laat_2\data\body_1_co.paa",
-                    "442_laat_2\data\body_2_co.paa",
-                    "442_laat_2\data\door_1_co.paa",
-                    "442_laat_2\data\door_2_co.paa",
-                    "442_laat_2\data\door_3_co.paa",
-                    "442_laat_2\data\wings_co.paa",
-                    "442_laat_2\data\missiles_co.paa",
-                    "442_laat_2\data\cockpits_co.paa",
-                    "442_laat_2\data\glass_ca.paa"
+                    "442_a_vehicle\laat\data\body_1_co.paa",
+                    "442_a_vehicle\laat\data\body_2_co.paa",
+                    "442_a_vehicle\laat\data\door_1_co.paa",
+                    "442_a_vehicle\laat\data\door_2_co.paa",
+                    "442_a_vehicle\laat\data\door_3_co.paa",
+                    "442_a_vehicle\laat\data\wings_co.paa",
+                    "442_a_vehicle\laat\data\missiles_co.paa",
+                    "442_a_vehicle\laat\data\cockpits_co.paa",
+                    "442_a_vehicle\laat\data\glass_ca.paa"
                 };
                 factions[] =
                 {
                     "kobra"
                 };
             };
+            class 44: base
+            {
+                displayname = "44th Attack Battalion";
+                textures[] = 
+                {
+                    "442_a_vehicle\laat\data\44\body_1_co.paa",
+                    "442_a_vehicle\laat\data\body_2_co.paa",
+                    "442_a_vehicle\laat\data\44\door_1_co.paa",
+                    "442_a_vehicle\laat\data\44\door_2_co.paa",
+                    "442_a_vehicle\laat\data\44\door_3_co.paa",
+                    "442_a_vehicle\laat\data\44\wings_co.paa",
+                    "442_a_vehicle\laat\data\missiles_co.paa",
+                    "442_a_vehicle\laat\data\cockpits_co.paa",
+                    "442_a_vehicle\laat\data\glass_ca.paa"
+                };
+            };
         };
         textureList[] =
         {
-            "base", 1
+            "base", 1,
+            "44", 1,
         };
