85fc7fdee8de0ea144d3f55ec1574fe69b3efcf7
kripto202 <kripto202@yahoo.com>
2020-11-16 12:04:47 -0500

    prepping folders

--- k_unit_ls/helmet.hpp
+++ k_unit_ls/helmet.hpp
@@ -1,236 +1,236 @@
 //////////clone helmets////////////
-class k_p2_helmet: k_p2_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    picture = "\k_unit_ls\data\ui\p2_helmet.paa";
-    hiddenselectionstextures[] = 
+    class k_p2_helmet: k_p2_helmet_base
     {
-        "k_unit_ls\data\helmet\p2_helmet\camo1_co.paa",
-        "k_unit_ls\data\helmet\p2_helmet\visor_co.paa"
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        picture = "\k_unit_ls\data\ui\p2_helmet.paa";
+        hiddenselectionstextures[] = 
+        {
+            "k_unit_ls\data\helmet\p2_helmet\camo1_co.paa",
+            "k_unit_ls\data\helmet\p2_helmet\visor_co.paa"
+        };
     };
-};
-class k_p15_helmet: k_p15_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    picture = "\k_unit_ls\data\ui\p15_helmet.paa";
-    hiddenselectionstextures[] = 
+    class k_p15_helmet: k_p15_helmet_base
     {
-        "k_unit_ls\data\helmet\p15_helmet\camo1_co.paa",
-        "k_unit_ls\data\helmet\p15_helmet\visor_co.paa",
-        "k_unit_ls\data\helmet\p15_helmet\illum_co.paa"
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        picture = "\k_unit_ls\data\ui\p15_helmet.paa";
+        hiddenselectionstextures[] = 
+        {
+            "k_unit_ls\data\helmet\p15_helmet\camo1_co.paa",
+            "k_unit_ls\data\helmet\p15_helmet\visor_co.paa",
+            "k_unit_ls\data\helmet\p15_helmet\illum_co.paa"
+        };
     };
-};
-class k_airborne_helmet: k_airborne_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    picture = "\k_unit_ls\data\ui\ab_helmet.paa";
-    hiddenselectionstextures[] = 
+    class k_airborne_helmet: k_airborne_helmet_base
     {
-        "k_unit_ls\data\helmet\ab_helmet\camo1_co.paa",
-        "k_unit_ls\data\helmet\ab_helmet\visor_co.paa"
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        picture = "\k_unit_ls\data\ui\ab_helmet.paa";
+        hiddenselectionstextures[] = 
+        {
+            "k_unit_ls\data\helmet\ab_helmet\camo1_co.paa",
+            "k_unit_ls\data\helmet\ab_helmet\visor_co.paa"
+        };
     };
-};
-class k_p2_pilot_helmet: k_p2_pilot_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    picture = "\k_unit_ls\data\ui\pilot_p2_helmet.paa";
-    hiddenselectionstextures[] = 
+    class k_p2_pilot_helmet: k_p2_pilot_helmet_base
     {
-        "k_unit_ls\data\helmet\p2_pilot_helmet\camo1_co.paa",
-        "k_unit_ls\data\helmet\p2_pilot_helmet\visor_co.paa" 
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        picture = "\k_unit_ls\data\ui\pilot_p2_helmet.paa";
+        hiddenselectionstextures[] = 
+        {
+            "k_unit_ls\data\helmet\p2_pilot_helmet\camo1_co.paa",
+            "k_unit_ls\data\helmet\p2_pilot_helmet\visor_co.paa" 
+        };
     };
-};
-class k_barc_helmet: k_barc_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    picture = "\k_unit_ls\data\ui\barc_helmet.paa";
-    hiddenselectionstextures[] =
+    class k_barc_helmet: k_barc_helmet_base
     {
-        "k_unit_ls\data\helmet\barc_helmet\camo1_co.paa",
-        "k_unit_ls\data\helmet\barc_helmet\visor_co.paa"  
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        picture = "\k_unit_ls\data\ui\barc_helmet.paa";
+        hiddenselectionstextures[] =
+        {
+            "k_unit_ls\data\helmet\barc_helmet\camo1_co.paa",
+            "k_unit_ls\data\helmet\barc_helmet\visor_co.paa"  
+        };
     };
-};
-class k_p1_arf_helmet: k_p1_arf_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    displayname = "[K] ARF Helmet";
-    picture = "\k_unit_ls\data\ui\arf_p1_helmet.paa";
-    /*hiddenselectionsmaterials[] = 
+    class k_p1_arf_helmet: k_p1_arf_helmet_base
     {
-        "k_unit_ls\data\helmet\arf_helmet\camo1.rvmat",
-        "",
-        "k_unit_ls\data\helmet\arf_helmet\visor.rvmat"
-    };*/
-    hiddenselectionstextures[] =
-    {
-        "k_unit_ls\data\helmet\arf_helmet\camo1_co.paa",
-        "",
-        "k_unit_ls\data\helmet\arf_helmet\visor_co.paa"  
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        displayname = "[K] ARF Helmet";
+        picture = "\k_unit_ls\data\ui\arf_p1_helmet.paa";
+        /*hiddenselectionsmaterials[] = 
+        {
+            "k_unit_ls\data\helmet\arf_helmet\camo1.rvmat",
+            "",
+            "k_unit_ls\data\helmet\arf_helmet\visor.rvmat"
+        };*/
+        hiddenselectionstextures[] =
+        {
+            "k_unit_ls\data\helmet\arf_helmet\camo1_co.paa",
+            "",
+            "k_unit_ls\data\helmet\arf_helmet\visor_co.paa"  
+        };
     };
-};
-class k_p2_arf_helmet: k_p2_arf_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    displayname = "[K] ARF Helmet w/ Flaps";
-    //model = "442_units\arf_p2_helmet.p3d";
-    picture = "\k_unit_ls\data\ui\arf_p2_helmet.paa";
-    /*hiddenselectionsmaterials[] = 
+    class k_p2_arf_helmet: k_p2_arf_helmet_base
     {
-        "k_unit_ls\data\helmet\arf_helmet\camo1.rvmat",
-        "k_unit_ls\data\helmet\arf_helmet\camo2.rvmat",
-        "k_unit_ls\data\helmet\arf_helmet\visor.rvmat"
-    };*/
-    hiddenselectionstextures[] = 
-    {
-        "k_unit_ls\data\helmet\arf_helmet\camo1_co.paa",
-        "k_unit_ls\data\helmet\arf_helmet\camo2_co.paa",
-        "k_unit_ls\data\helmet\arf_helmet\visor_co.paa"
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        displayname = "[K] ARF Helmet w/ Flaps";
+        //model = "442_units\arf_p2_helmet.p3d";
+        picture = "\k_unit_ls\data\ui\arf_p2_helmet.paa";
+        /*hiddenselectionsmaterials[] = 
+        {
+            "k_unit_ls\data\helmet\arf_helmet\camo1.rvmat",
+            "k_unit_ls\data\helmet\arf_helmet\camo2.rvmat",
+            "k_unit_ls\data\helmet\arf_helmet\visor.rvmat"
+        };*/
+        hiddenselectionstextures[] = 
+        {
+            "k_unit_ls\data\helmet\arf_helmet\camo1_co.paa",
+            "k_unit_ls\data\helmet\arf_helmet\camo2_co.paa",
+            "k_unit_ls\data\helmet\arf_helmet\visor_co.paa"
+        };
     };
-};
-class k_eng_helmet: k_eng_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    picture = "\k_unit_ls\data\ui\eng_helmet.paa";
-    hiddenselectionstextures[] = 
+    class k_eng_helmet: k_eng_helmet_base
     {
-        "k_unit_ls\data\helmet\eng_helmet\camo1_co.paa",
-        "k_unit_ls\data\helmet\eng_helmet\light_co.paa",
-        "k_unit_ls\data\helmet\eng_helmet\visor_co.paa"
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        picture = "\k_unit_ls\data\ui\eng_helmet.paa";
+        hiddenselectionstextures[] = 
+        {
+            "k_unit_ls\data\helmet\eng_helmet\camo1_co.paa",
+            "k_unit_ls\data\helmet\eng_helmet\light_co.paa",
+            "k_unit_ls\data\helmet\eng_helmet\visor_co.paa"
+        };
     };
-};
-class k_p1_helmet: k_p1_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    picture = "\k_unit_ls\data\ui\p1_helmet.paa";
-    hiddenselectionstextures[] =
+    class k_p1_helmet: k_p1_helmet_base
     {
-        "k_unit_ls\data\helmet\p1_helmet\camo1_co.paa",
-        "k_unit_ls\data\helmet\p1_helmet\visor_co.paa",
-        "k_unit_ls\data\helmet\p1_helmet\illum_co.paa",
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        picture = "\k_unit_ls\data\ui\p1_helmet.paa";
+        hiddenselectionstextures[] =
+        {
+            "k_unit_ls\data\helmet\p1_helmet\camo1_co.paa",
+            "k_unit_ls\data\helmet\p1_helmet\visor_co.paa",
+            "k_unit_ls\data\helmet\p1_helmet\illum_co.paa",
+        };
     };
-};
-class k_p1_pilot_helmet: k_p1_pilot_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    picture = "\k_unit_ls\data\ui\pilot_p1_helmet.paa";
-    hiddenselectionstextures[] = 
+    class k_p1_pilot_helmet: k_p1_pilot_helmet_base
     {
-        "k_unit_ls\data\helmet\p1_pilot_helmet\camo1_co.paa",
-        "k_unit_ls\data\helmet\p1_pilot_helmet\camo2_co.paa",
-        "k_unit_ls\data\helmet\p1_pilot_helmet\visor_co.paa",
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        picture = "\k_unit_ls\data\ui\pilot_p1_helmet.paa";
+        hiddenselectionstextures[] = 
+        {
+            "k_unit_ls\data\helmet\p1_pilot_helmet\camo1_co.paa",
+            "k_unit_ls\data\helmet\p1_pilot_helmet\camo2_co.paa",
+            "k_unit_ls\data\helmet\p1_pilot_helmet\visor_co.paa",
+        };
     };
-};
-class k_p1_specops_helmet: k_p1_specops_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    picture = "\k_unit_ls\data\ui\spec_p1_helmet.paa";
-    hiddenselectionstextures[] = 
+    class k_p1_specops_helmet: k_p1_specops_helmet_base
     {
-        "k_unit_ls\data\helmet\p1_spec_helmet\camo1_co.paa",
-        "k_unit_ls\data\helmet\p1_spec_helmet\visor_co.paa",
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        picture = "\k_unit_ls\data\ui\spec_p1_helmet.paa";
+        hiddenselectionstextures[] = 
+        {
+            "k_unit_ls\data\helmet\p1_spec_helmet\camo1_co.paa",
+            "k_unit_ls\data\helmet\p1_spec_helmet\visor_co.paa",
+        };
     };
-};
-class k_p2_specops_helmet: k_p2_specops_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    picture = "\k_unit_ls\data\ui\spec_p2_helmet.paa";
-    hiddenselectionstextures[] = 
+    class k_p2_specops_helmet: k_p2_specops_helmet_base
     {
-        "k_unit_ls\data\helmet\p2_spec_helmet\camo1_co.paa",
-        "k_unit_ls\data\helmet\p2_spec_helmet\visor_co.paa",
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        picture = "\k_unit_ls\data\ui\spec_p2_helmet.paa";
+        hiddenselectionstextures[] = 
+        {
+            "k_unit_ls\data\helmet\p2_spec_helmet\camo1_co.paa",
+            "k_unit_ls\data\helmet\p2_spec_helmet\visor_co.paa",
+        };
     };
-};
-class k_rex_helmet: k_rex_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    picture = "\k_unit_ls\data\ui\rex_helmet.paa";
-    hiddenselectionstextures[] =
+    class k_rex_helmet: k_rex_helmet_base
     {
-        "k_unit_ls\data\helmet\rex_helmet\camo1_co.paa",
-        "k_unit_ls\data\helmet\rex_helmet\visor_co.paa" 
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        picture = "\k_unit_ls\data\ui\rex_helmet.paa";
+        hiddenselectionstextures[] =
+        {
+            "k_unit_ls\data\helmet\rex_helmet\camo1_co.paa",
+            "k_unit_ls\data\helmet\rex_helmet\visor_co.paa" 
+        };
     };
-};
 
 ////////b1 helmets//////////
-class k_b1_helmet: k_b1_helmet_base
-{
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    displayname = "[K] B1 Droid Head";
-    picture = "\ls_armor_redfor\uniform\cis\ui\icon_b1_uniform_ca.paa";
-    hiddenselectionsmaterials[] = 
+    class k_b1_helmet: k_b1_helmet_base
     {
-        "442_units\cis_droids\data\camo1.rvmat"
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        displayname = "[K] B1 Droid Head";
+        picture = "\ls_armor_redfor\uniform\cis\ui\icon_b1_uniform_ca.paa";
+        hiddenselectionsmaterials[] = 
+        {
+            "442_units\cis_droids\data\camo1.rvmat"
+        };
+        hiddenselectionstextures[] = 
+        {
+            "442_units\cis_droids\data\b1_upper_co.paa"
+        };
     };
-    hiddenselectionstextures[] = 
+    class k_b1_helmet_geonosis: k_b1_helmet
     {
-        "442_units\cis_droids\data\b1_upper_co.paa"
+        displayname = "[K] B1 Droid Head (Geonosis)";
+        hiddenselectionstextures[] = 
+        {
+            "442_units\cis_droids\data\b1_upper_geonosis_co.paa"
+        };
     };
-};
-class k_b1_helmet_geonosis: k_b1_helmet
-{
-    displayname = "[K] B1 Droid Head (Geonosis)";
-    hiddenselectionstextures[] = 
+    class k_b1_helmet_cmd: k_b1_helmet
     {
-        "442_units\cis_droids\data\b1_upper_geonosis_co.paa"
+        displayname = "[K] B1 Droid Head (Commander)";
+        hiddenselectionstextures[] = 
+        {
+            "442_units\cis_droids\data\b1_upper_cmd_co.paa"
+        };
     };
-};
-class k_b1_helmet_cmd: k_b1_helmet
-{
-    displayname = "[K] B1 Droid Head (Commander)";
-    hiddenselectionstextures[] = 
+    class k_b1_helmet_woodland: k_b1_helmet
     {
-        "442_units\cis_droids\data\b1_upper_cmd_co.paa"
+        displayname = "[K] B1 Droid Head (Woodland)";
+        hiddenselectionstextures[] = 
+        {
+            "442_units\cis_droids\data\b1_upper_woodland_co.paa"
+        };
     };
-};
-class k_b1_helmet_woodland: k_b1_helmet
-{
-    displayname = "[K] B1 Droid Head (Woodland)";
-    hiddenselectionstextures[] = 
+    class k_b1_helmet_desert: k_b1_helmet
     {
-        "442_units\cis_droids\data\b1_upper_woodland_co.paa"
+        displayname = "[K] B1 Droid Head (Desert)";
+        hiddenselectionstextures[] = 
+        {
+            "442_units\cis_droids\data\b1_upper_desert_co.paa"
+        };
     };
-};
-class k_b1_helmet_desert: k_b1_helmet
-{
-    displayname = "[K] B1 Droid Head (Desert)";
-    hiddenselectionstextures[] = 
+    class k_b1_helmet_winter: k_b1_helmet
     {
-        "442_units\cis_droids\data\b1_upper_desert_co.paa"
+        displayname = "[K] B1 Droid Head (Winter)";
+        hiddenselectionstextures[] = 
+        {
+            "442_units\cis_droids\data\b1_upper_winter_co.paa"
+        };
     };
-};
-class k_b1_helmet_winter: k_b1_helmet
-{
-    displayname = "[K] B1 Droid Head (Winter)";
-    hiddenselectionstextures[] = 
-    {
-        "442_units\cis_droids\data\b1_upper_winter_co.paa"
-    };
-};
--- k_unit_ls/uniform.hpp
+++ k_unit_ls/uniform.hpp
@@ -1,106 +1,92 @@
 //////////clone uniforms
-class k_swla_uniform: k_swla_uniform_base
-{
-    displayname = "[K] Clone Trooper Armor";
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    class ItemInfo: ItemInfo
+    class k_swla_uniform: k_swla_uniform_base
     {
-        uniformclass = "k_swla_unit_base";
+        displayname = "[K] Clone Trooper Armor";
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        class ItemInfo: ItemInfo
+        {
+            uniformclass = "k_swla_unit_base";
+        };
     };
-};
-class k_swla_mc_uniform: k_swla_uniform_mc_base
-{
-    displayname = "[K] Clone Marshall Commander Armor";
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    class ItemInfo: ItemInfo
+    class k_swla_mc_uniform: k_swla_uniform_mc_base
     {
-        uniformclass = "k_swla_unit_mc_base";
+        displayname = "[K] Clone Marshall Commander Armor";
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        class ItemInfo: ItemInfo
+        {
+            uniformclass = "k_swla_unit_mc_base";
+        };
     };
-};
 
 ////////b1 uniforms///////
-class k_swla_b1_uniform: k_b1_uniform_base
-{
-    displayname = "[K] B1 Uniform";
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    class ItemInfo: Iteminfo
+    class k_swla_b1_uniform: k_b1_uniform_base
     {
-        uniformclass = "k_swla_unit_b1_base";
+        displayname = "[K] B1 Uniform";
+        scope = 2;
+        scopecurator = 2;
+        scopearsenal = 2;
+        class ItemInfo: Iteminfo
+        {
+            uniformclass = "k_swla_unit_b1_base";
+        };
     };
-};
-class k_swla_b1_geonosis_uniform: k_swla_b1_uniform
-{
-    displayname = "[K] B1 Uniform (Geonosis)";
-    class ItemInfo: ItemInfo
+    class k_swla_b1_geonosis_uniform: k_swla_b1_uniform
     {
-        uniformclass = "k_swla_unit_b1_geonosis_base";
+        displayname = "[K] B1 Uniform (Geonosis)";
+        class ItemInfo: ItemInfo
+        {
+            uniformclass = "k_swla_unit_b1_geonosis_base";
+        };
     };
-};
-class k_swla_b1_security_uniform: k_swla_b1_uniform
-{
-    displayname = "[K] B1 Uniform (Security)";
-    class ItemInfo: ItemInfo
+    class k_swla_b1_security_uniform: k_swla_b1_uniform
     {
-        uniformclass = "k_swla_unit_b1_security_base";
+        displayname = "[K] B1 Uniform (Security)";
+        class ItemInfo: ItemInfo
+        {
+            uniformclass = "k_swla_unit_b1_security_base";
+        };
     };
-};
-class k_swla_b1_pilot_uniform: k_swla_b1_uniform
-{
-    displayname = "[K] B1 Uniform (Pilot)";
-    class ItemInfo: ItemInfo
+    class k_swla_b1_pilot_uniform: k_swla_b1_uniform
     {
-        uniformclass = "k_swla_unit_b1_pilot_base";
+        displayname = "[K] B1 Uniform (Pilot)";
+        class ItemInfo: ItemInfo
+        {
+            uniformclass = "k_swla_unit_b1_pilot_base";
+        };
     };
-};
-class k_swla_b1_cmd_uniform: k_swla_b1_uniform
-{
-    displayname = "[K] B1 Uniform (Commander)";
-    class ItemInfo: ItemInfo
+    class k_swla_b1_cmd_uniform: k_swla_b1_uniform
     {
-        uniformclass = "k_swla_unit_b1_cmd_base";
+        displayname = "[K] B1 Uniform (Commander)";
+        class ItemInfo: ItemInfo
+        {
+            uniformclass = "k_swla_unit_b1_cmd_base";
+        };
     };
-};
-class k_swla_b1_woodland_uniform: k_swla_b1_uniform
-{
-    displayname = "[K] B1 Uniform (Woodland)";
-    class ItemInfo: ItemInfo
+    class k_swla_b1_woodland_uniform: k_swla_b1_uniform
     {
-        uniformclass = "k_swla_unit_b1_woodland_base";
+        displayname = "[K] B1 Uniform (Woodland)";
+        class ItemInfo: ItemInfo
+        {
+            uniformclass = "k_swla_unit_b1_woodland_base";
+        };
     };
-};
-class k_swla_b1_desert_uniform: k_swla_b1_uniform
-{
-    displayname = "[K] B1 Uniform (Desert)";
-    class ItemInfo: ItemInfo
+    class k_swla_b1_desert_uniform: k_swla_b1_uniform
     {
-        uniformclass = "k_swla_unit_b1_desert_base";
+        displayname = "[K] B1 Uniform (Desert)";
+        class ItemInfo: ItemInfo
+        {
+            uniformclass = "k_swla_unit_b1_desert_base";
+        };
     };
-};
-class k_swla_b1_winter_uniform: k_swla_b1_uniform
-{
-    displayname = "[K] B1 Uniform (Winter)";
-    class ItemInfo: ItemInfo
+    class k_swla_b1_winter_uniform: k_swla_b1_uniform
     {
-        uniformclass = "k_swla_unit_b1_winter_base";
+        displayname = "[K] B1 Uniform (Winter)";
+        class ItemInfo: ItemInfo
+        {
+            uniformclass = "k_swla_unit_b1_winter_base";
+        };
     };
-};
-class k_barracks_uniform: U_O_officer_noinsignia_hex_F
-{
-    displayname = "Barracks Uniform"; //uniform name
-    scope = 2;
-    scopecurator = 2;
-    scopearsenal = 2;
-    class ItemInfo: UniformItem
-    {
-        uniformmodel = "-";
-        containerclass = "Supply50"; //how much the uniform can store stuff
-        mass = 20; //how heavy the uniform is
-        uniformclass = "k_barracks_unit"; //needs to link to a unit 
-    };
-};
