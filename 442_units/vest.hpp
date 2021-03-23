1398a0f67cc32e86c9982fb27e2d6bf182f1c3c3
kripto202 <kripto202@yahoo.com>
2021-03-23 10:43:54 -0400

    add kama

--- 442_units/vest.hpp
+++ 442_units/vest.hpp
@@ -84,6 +84,74 @@
             mass = 20;
         };
     };
+	
+	class k_kama_base: k_vest_base
+	{
+		author = "KOBRA Mod Team";
+		displayname = "Kama";
+		model = "kobra\442_units\kama.p3d";
+		hiddenselections[] = 
+		{
+			"kamaL",
+			"kamaR"
+		};
+		class ItemInfo: VestItem
+		{
+			hiddenselections[] = 
+			{
+				"kamaL",
+				"kamaR"
+			};
+			uniformmodel = "kobra\442_units\kama.p3d";
+			containerclass = "Supply100";
+			mass = 20;
+			class HitpointsProtectionInfo
+            {
+                class Abdomen
+                {
+                    hitpointName = "HitAbdomen";
+                    armor = 1;
+                    passThrough = 0.3;
+                };
+                class Body
+                {
+                    hitpointName = "HitBody";
+                    passThrough = 0.3;
+                };
+            };
+		};
+	};
+	
+	class k_kama_full: k_kama_base
+	{
+		displayname = "Kama";
+		scope = 2;
+		scopecurator = 2;
+		scopearsenal = 2;
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama_co.paa",
+			"kobra\442_units\data\vests\kama_co.paa"
+		};
+	};
+	class k_kama_L: k_kama_full
+	{
+		displayname = "Left Kama";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama_co.paa",
+			""
+		};
+	};
+	class k_kama_R: k_kama_full
+	{
+		displayname = "Right Kama";
+		hiddenselectionstextures[] = 
+		{
+			"",
+			"kobra\442_units\data\vests\kama_co.paa"
+		};
+	};
 
 ////////////designs///////////
     class k_scout_pouches_light_dukas: k_scout_pouches_light
@@ -238,3 +306,171 @@
             "kobra\442_units\data\vests\pouches_desert_co.paa"
         };
     };
+	
+	class k_kama_dirt: k_kama_full
+	{
+		displayname = "Kama Dirty";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama_dirt_co.paa",
+			"kobra\442_units\data\vests\kama_dirt_co.paa"
+		};
+	};
+	class k_kama_dirtL: k_kama_full
+	{
+		displayname = "Left Kama Dirty";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama_dirt_co.paa",
+			""
+		};
+	};
+	class k_kama_dirtR: k_kama_full
+	{
+		displayname = "Right Kama Dirty";
+		hiddenselectionstextures[] = 
+		{
+			"",
+			"kobra\442_units\data\vests\kama_dirt_co.paa"
+		};
+	};
+	
+	class k_kama_1: k_kama_full
+	{
+		displayname = "Kama 1";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama1_co.paa",
+			"kobra\442_units\data\vests\kama1_co.paa"
+		};
+	};
+	class k_kama_1L: k_kama_full
+	{
+		displayname = "Left Kama 1";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama1_co.paa",
+			""
+		};
+	};
+	class k_kama_1R: k_kama_full
+	{
+		displayname = "Right Kama 1";
+		hiddenselectionstextures[] = 
+		{
+			"",
+			"kobra\442_units\data\vests\kama1_co.paa"
+		};
+	};
+	
+	class k_kama_2: k_kama_full
+	{
+		displayname = "Kama 2";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama2_co.paa",
+			"kobra\442_units\data\vests\kama2_co.paa"
+		};
+	};
+	class k_kama_2L: k_kama_full
+	{
+		displayname = "Left Kama 2";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama2_co.paa",
+			""
+		};
+	};
+	class k_kama_2R: k_kama_full
+	{
+		displayname = "Right Kama 2";
+		hiddenselectionstextures[] = 
+		{
+			"",
+			"kobra\442_units\data\vests\kama2_co.paa"
+		};
+	};
+	
+	class k_kama_5: k_kama_full
+	{
+		displayname = "Kama 5";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama5_co.paa",
+			"kobra\442_units\data\vests\kama5_co.paa"
+		};
+	};
+	class k_kama_5L: k_kama_full
+	{
+		displayname = "Left Kama 5";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama5_co.paa",
+			""
+		};
+	};
+	class k_kama_5R: k_kama_full
+	{
+		displayname = "Right Kama 5";
+		hiddenselectionstextures[] = 
+		{
+			"",
+			"kobra\442_units\data\vests\kama5_co.paa"
+		};
+	};
+	
+	class k_kama_6: k_kama_full
+	{
+		displayname = "Kama 6";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama6_co.paa",
+			"kobra\442_units\data\vests\kama6_co.paa"
+		};
+	};
+	class k_kama_6L: k_kama_full
+	{
+		displayname = "Left Kama 6";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama6_co.paa",
+			""
+		};
+	};
+	class k_kama_6R: k_kama_full
+	{
+		displayname = "Right Kama 6";
+		hiddenselectionstextures[] = 
+		{
+			"",
+			"kobra\442_units\data\vests\kama6_co.paa"
+		};
+	};
+	
+	class k_kama_7: k_kama_full
+	{
+		displayname = "Kama 7";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama7_co.paa",
+			"kobra\442_units\data\vests\kama7_co.paa"
+		};
+	};
+	class k_kama_7L: k_kama_full
+	{
+		displayname = "Left Kama 7";
+		hiddenselectionstextures[] = 
+		{
+			"kobra\442_units\data\vests\kama7_co.paa",
+			""
+		};
+	};
+	class k_kama_7R: k_kama_full
+	{
+		displayname = "Right Kama 7";
+		hiddenselectionstextures[] = 
+		{
+			"",
+			"kobra\442_units\data\vests\kama7_co.paa"
+		};
+	};
